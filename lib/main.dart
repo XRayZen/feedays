c1147654d982981a3b71f6d723c589fb50106d30
加藤原語 <31905238+XRayZen@users.noreply.github.com>
2023-01-23 03:44:56 +0900

    test delete branch

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        
+        // Try running your application with "flutter run". You'll see the
+        //からの変更をマージします。
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
