37690b10cbbe4a1d15b283d43f4775ba0a8344cc
加藤原語 <31905238+XRayZen@users.noreply.github.com>
2023-01-29 05:22:25 +0900

    fix

--- lib/ui/widgets/reorderable_tree_view.dart
+++ lib/ui/widgets/reorderable_tree_view.dart
@@ -24,66 +24,73 @@ class _ReorderableTreeListViewState
       //カテゴリータイルにドラッグアンドドロップしたらそれを特定して
       //入れ替え処理を行う
       //編集モードの切替で右にメニューアイコンを表示させてドラッグ可能か判断できるようにする
-      //NOTE:DragAndDropListsはページなら静的な高さは要らないがドロワーメニューでは静的な高さ設定が必要
+      //NOTE:`DragAndDropLists`はページなら静的な高さは要らないが、ドロワーメニューでは静的な高さ設定が必要
       return DragAndDropLists(
-          children: _buildList(ref.watch(feedsSiteListProvider)),
-          onItemReorder: _onChildItemReorder,
-          onListReorder: _onListReorder,
-          listPadding: const EdgeInsets.all(5),
-          itemDivider: Divider(
-            thickness: 2,
-            height: 2,
-            color: Theme.of(context).scaffoldBackgroundColor,
-          ),
-          listInnerDecoration: BoxDecoration(
-            color: Theme.of(context).canvasColor,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          itemDecorationWhileDragging: BoxDecoration(
-            // color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.red.withOpacity(0.4),
-                spreadRadius: 2,
-                blurRadius: 2,
-                offset: Offset(1, 0), // changes position of shadow
+        children: _buildList(ref.watch(feedsSiteListProvider)),
+        onItemReorder: _onChildItemReorder,
+        onListReorder: _onListReorder,
+        listPadding: const EdgeInsets.all(5),
+        itemDivider: Divider(
+          thickness: 2,
+          height: 2,
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        listInnerDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        itemDecorationWhileDragging: BoxDecoration(
+          // color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.red.withOpacity(0.4),
+              spreadRadius: 2,
+              blurRadius: 2,
+              offset: Offset(1, 0), // changes position of shadow
+            ),
+          ],
+        ),
+        listGhost: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          child: Center(
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 100.0),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(7.0),
               ),
-            ],
-          ),
-          listGhost: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30.0),
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 30.0, horizontal: 100.0),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(7.0),
-                ),
-                child: const Icon(Icons.add_box),
-              ),
+              child: const Icon(Icons.add_box),
             ),
           ),
-          itemGhost: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 2.0),
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 30.0, horizontal: 10.0),
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-                child: const Icon(Icons.add_box),
+        ),
+        itemGhost: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Center(
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                borderRadius: BorderRadius.circular(25.0),
               ),
+              child: const Icon(Icons.add_box),
             ),
           ),
-          lastItemTargetHeight: 10,
-          addLastItemTargetHeightToTop: true,
-          lastListTargetSize: 40,
-          listDragHandle: _buildDragHandle(isList: true),
-          itemDragHandle: _buildDragHandle());
+        ),
+        lastItemTargetHeight: 10,
+        addLastItemTargetHeightToTop: true,
+        lastListTargetSize: 40,
+        listDragHandle: _buildDragHandle(isList: true),
+        itemDragHandle: _buildDragHandle(),
+        itemOnAccept: _dragItemAccept,
+      );
     }
+  }
+
+  //ドラッグされて投下されたら
+  void _dragItemAccept(DragAndDropItem incoming,DragAndDropItem target) {
+
   }
 
   _buildDragHandle({bool isList = false}) {
@@ -122,17 +129,20 @@ class _ReorderableTreeListViewState
           listKey: ObjectKey(node),
           title: Text('${node.name}'),
           leading: const Icon(Icons.ac_unit),
-          children: _buildTreeChileNodes(node.nodes)));
+          children: _buildTreeChilledNode(node.nodes)));
     }
     return widgets;
   }
 
-  List<DragAndDropItem> _buildTreeChileNodes(List<FeedModel> models) {
+  List<DragAndDropItem> _buildTreeChilledNode(List<FeedModel> models) {
     if (models.isEmpty) {
       return [];
     }
     return models.map((e) {
       return DragAndDropItem(
+        //ドラッグしてるときの様子
+        // feedbackWidget: ,
+        //PLAN:後回し
         child: ListTile(
           title: Text(
             e.name,
